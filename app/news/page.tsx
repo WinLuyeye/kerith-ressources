568f96be72deabe714d2bb0c968bcff5daf32fd0
Winner luyeye <winnerluyeye1@gmail.com>
2026-04-03 23:25:32 +0100

    feat: fixe hero section

---
+++ app/news/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewsPage() {
+  return (
+    <div>
+      <h1>Actualités</h1>
+      <p>Contenu de la page news ici...</p>
+    </div>
+  );
+}
